6705b877800b205f1b742b7255de1b717892c9cf
Tajgi Fields <Tajgi@icloud.com>
2025-11-11 23:06:22 -0500

    fix double auth error on users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { updateUser } = require("../controllers/update");
 const { getCurrentUser } = require("../controllers/users");
-const auth = require("../middlewares/auth");
 
-router.use(auth);
+
+
 router.get("/me", getCurrentUser);
 router.patch("/me", updateUser);
 
