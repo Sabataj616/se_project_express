e08ae9fea1b304903c4fb5a89fb99d25430ccc9f
Tajgi Fields <Tajgi@icloud.com>
2025-12-16 18:50:58 -0500

    added random crypto key instead of old jwt secret key

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,4 @@
-const { JWT_SECRET = "fantastic-four-key-828" } = process.env;
+const {
+  JWT_SECRET = "b4ab7e922c262c9dd3f794267748f772dbb2e4d3b788731d779fc552281883fe",
+} = process.env;
 module.exports = { JWT_SECRET };
